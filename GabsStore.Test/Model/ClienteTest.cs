b19479bb7e818419ed6a023a876da636937ab220
Gabriel <Gabriel@DESKTOP-L6PO50G>
2022-07-26 09:44:04 -0300

    initial

---
+++ GabsStore.Test/Model/ClienteTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GabsStore.Test.Model
+{
+	public class ClienteTest
+	{
+		[Fact]
+		public void ToDo()
+		{
+		}
+	}
+}
